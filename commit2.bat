b4cef8ffc61a8624ee43811a46c6aa9df24642a6
Danu <dciptadi@gmail.com>
2025-08-27 10:15:37 +0700

    update Wed 08 27

---
+++ commit2.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+
+:: Get current date in format "25 August 2025"
+for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
+    set day=%%a
+    set month=%%b
+    set year=%%c
+)
+
+:: Create commit message with English month
+set commit_message=update %day% %month% %year%
+
+:: Execute Git commands
+echo Running Git commands...
+git add .
+git commit -m "%commit_message%"
+git push origin sidapet_statis
+
+echo.
+echo Commit message: %commit_message%
+echo Done!
+
+endlocal
