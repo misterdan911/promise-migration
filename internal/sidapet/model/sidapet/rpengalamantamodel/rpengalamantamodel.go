2e1b8a886cace53d28ac31288658ef622ff99e46
Danu <dciptadi@gmail.com>
2025-08-31 13:34:16 +0700

    update Sun 08 31

--- internal/sidapet/model/sidapet/rpengalamantamodel/rpengalamantamodel.go
+++ internal/sidapet/model/sidapet/rpengalamantamodel/rpengalamantamodel.go
@@ -4,34 +4,35 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"github.com/jackc/pgx/v5"
 	"promise-migration/db"
 	"promise-migration/internal/sidapet/structs"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func InsertRefPengalamanTa(kodeTA int32, vTPP structs.VmsTblPersonaliaPerush) {
 
-	ctx := context.Background()
+ ctx := context.Background()
 
-	qIns := `
-		INSERT INTO ref_pengalaman_ta (
-		  kode_tenaga_ahli,
-		  pengalaman,
-		  file_bukti
-		) VALUES (
-		  @kode_tenaga_ahli,
-		  @pengalaman,
-		  @file_bukti
-		)`
+ qIns := `
+  INSERT INTO ref_pengalaman_ta (
+    kode_tenaga_ahli,
+    pengalaman,
+    file_bukti
+  ) VALUES (
+    @kode_tenaga_ahli,
+    @pengalaman,
+    @file_bukti
+  )`
 
-	args := pgx.NamedArgs{
-		"kode_tenaga_ahli": sql.NullInt32{Valid: true, Int32: kodeTA},
-		"pengalaman":       vTPP.PengalamanPersonal,
-		"file_bukti":       sql.NullString{},
-	}
+ args := pgx.NamedArgs{
+  "kode_tenaga_ahli": sql.NullInt32{Valid: true, Int32: kodeTA},
+  "pengalaman":       vTPP.PengalamanPersonal,
+  "file_bukti":       sql.NullString{},
+ }
 
-	_, errIns := db.DbSidapet.Exec(ctx, qIns, args)
-	if errIns != nil {
-		fmt.Println("unable to insert ref_pengalaman_ta, " + errIns.Error())
-	}
+ _, errIns := db.DbSidapet.Exec(ctx, qIns, args)
+ if errIns != nil {
+  fmt.Println("unable to insert ref_pengalaman_ta, " + errIns.Error())
+ }
 }
--- internal/sidapet/model/sidapet/rtenagaahlibumodel/rtenagaahlibumodel.go
+++ internal/sidapet/model/sidapet/rtenagaahlibumodel/rtenagaahlibumodel.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"promise-migration/db"
+	"promise-migration/internal/model/dbsidapet/helperdokumenmodel"
 	"promise-migration/internal/model/dbsidapet/helperjenjangpendidikankwmodel"
 	"promise-migration/internal/model/dbsidapet/helperusermodel"
 	"promise-migration/internal/sidapet/model/sidapet/rpengalamantamodel"
@@ -23,26 +24,37 @@ import (
 )
 
 type RefTenagaAhli struct {
- KodeTenagaAhli pgtype.Int4
+	KodeTenagaAhli pgtype.Int4
 }
 type RefTenagaPendukung struct {
- KodeTenagaPendukung pgtype.Int4
+	KodeTenagaPendukung pgtype.Int4
 }
 
 var helperUser helperusermodel.HelperUser
 
+//var IdPersonalia int32
+
 func InsertPersonalia(profilePenyedia structs2.TblProfilePenyedia, userHelper helperusermodel.HelperUser) {
 
- helperUser = userHelper
+	helperUser = userHelper
 
- if profilePenyedia.IdJenisPenyedia.Int32 == 2 {
-  return
- }
+	if profilePenyedia.IdJenisPenyedia.Int32 == 2 {
+		return
+	}
 
- ctx := context.Background()
+	ctx := context.Background()
 
- qVmsTblPersonaliaP := `
- SELECT
+  var dbTable string
+
+  if helperUser.DbPenyedia.String == "vms_db" {
+    dbTable = "tbl_personalia_perusahaan"
+  }
+  if helperUser.DbPenyedia.String == "promise_sibela" {
+    dbTable = "tbl_personalia_perusahaan1"
+  }
+
+	qVmsTblPersonaliaP := `
+  SELECT
    id_personalia,
    id_profil_penyedia,
    nm_personal,
@@ -53,43 +65,63 @@ func InsertPersonalia(profilePenyedia structs2.TblProfilePenyedia, userHelper he
    keahlian_personal,
    sertif_personal,
    path_personal
- FROM tbl_personalia_perusahaan
- WHERE id_profil_penyedia = $1`
+  FROM ` + dbTable + `
+  WHERE id_profil_penyedia = $1`
 
- rVTPP, errVTPP := db.VmsDb.Query(ctx, qVmsTblPersonaliaP, strconv.Itoa(int(profilePenyedia.IdProfilPenyedia.Int32)))
- if errVTPP != nil {
-  log.Fatal("qVmsTblPersonaliaP Failed, " + errVTPP.Error() + " " + qVmsTblPersonaliaP)
- }
+  var rVTPP pgx.Rows
+  var errVTPP error
 
- allVTPP, errCollect := pgx.CollectRows(rVTPP, pgx.RowToStructByName[structs.VmsTblPersonaliaPerush])
- if errCollect != nil {
-  log.Fatal("failed collecting rows, " + errCollect.Error())
- }
- defer rVTPP.Close()
-
- for _, vTPP := range allVTPP {
-  if strings.Contains(strings.ToLower(vTPP.JbtnPersonal.String), "pendukung") {
-   InsertRefTenagaPendukungBu(vTPP)
-  } else {
-   InsertRefTenagaAhliBu(vTPP)
+  if helperUser.DbPenyedia.String == "vms_db" {
+    rVTPP, errVTPP = db.VmsDb.Query(ctx, qVmsTblPersonaliaP, strconv.Itoa(int(profilePenyedia.IdProfilPenyedia.Int32)))
   }
- }
+  if helperUser.DbPenyedia.String == "promise_sibela" {
+    rVTPP, errVTPP = db.PromiseSibela.Query(ctx, qVmsTblPersonaliaP, strconv.Itoa(int(profilePenyedia.IdProfilPenyedia.Int32)))
+  }
+
+	// rVTPP, errVTPP := db.VmsDb.Query(ctx, qVmsTblPersonaliaP, strconv.Itoa(int(profilePenyedia.IdProfilPenyedia.Int32)))
+
+	if errVTPP != nil {
+		log.Fatal("qVmsTblPersonaliaP Failed, " + errVTPP.Error() + " " + qVmsTblPersonaliaP)
+	}
+
+	allVTPP, errCollect := pgx.CollectRows(rVTPP, pgx.RowToStructByName[structs.VmsTblPersonaliaPerush])
+	if errCollect != nil {
+		log.Fatal("failed collecting rows, " + errCollect.Error())
+	}
+	defer rVTPP.Close()
+
+	for _, vTPP := range allVTPP {
+
+		//fmt.Printf("IdPersonalia: %d\n", vTPP.IdPersonalia.Int32)
+
+		//IdPersonalia = vTPP.IdPersonalia.Int32
+
+		if strings.Contains(strings.ToLower(vTPP.JbtnPersonal.String), "pendukung") {
+			InsertRefTenagaPendukungBu(vTPP)
+		} else {
+			InsertRefTenagaAhliBu(vTPP)
+		}
+	}
 
 }
 
 func InsertRefTenagaAhliBu(vTPP structs.VmsTblPersonaliaPerush) {
 
- ctx := context.Background()
+	ctx := context.Background()
 
- var tglLahir pgtype.Text
- strTglLahir, _ := sidapethelper.ConvertToPostgresDate(vTPP.TglPersonal.String)
- if strTglLahir != "" {
-  tglLahir = pgtype.Text{Valid: true, String: strTglLahir}
- }
+	var tglLahir pgtype.Text
+	strTglLahir, _ := sidapethelper.ConvertToPostgresDate(vTPP.TglPersonal.String)
+	if strTglLahir != "" {
+		tglLahir = pgtype.Text{Valid: true, String: strTglLahir}
+	}
 
- kodeJenjangPendidikan := GetKodeJenjangPendidikan(vTPP.PendidikanPersonal)
+	kodeJenjangPendidikan := GetKodeJenjangPendidikan(vTPP.PendidikanPersonal)
 
- qIns := `
+  helperDokumen := helperdokumenmodel.GetByOriginalPath(vTPP.PathPersonal)
+  fileIjazah := helperDokumen.Newfilename
+  encryptKeyIjazah := helperDokumen.EncryptKey
+
+	qIns := `
   INSERT INTO ref_tenaga_ahli_bu (
     kode_vendor,
     nama,
@@ -101,6 +133,7 @@ func InsertRefTenagaAhliBu(vTPP structs.VmsTblPersonaliaPerush) {
     kode_jenjang_pendidikan,
     program_studi,
     file_ijazah,
+    encrypt_key_ijazah,
     file_cv
   ) VALUES (
     @kode_vendor,
@@ -113,57 +146,63 @@ func InsertRefTenagaAhliBu(vTPP structs.VmsTblPersonaliaPerush) {
     @kode_jenjang_pendidikan,
     @program_studi,
     @file_ijazah,
+    @encrypt_key_ijazah,
     @file_cv
   ) RETURNING kode_tenaga_ahli`
 
- args := pgx.NamedArgs{
-  "kode_vendor":             helperUser.KodeVendor,
-  "nama":                    vTPP.NmPersonal,
-  "no_ktp":                  sql.NullString{},
-  "file_ktp":                sql.NullString{},
-  "tempat_lahir":            sql.NullString{},
-  "tgl_lahir":               tglLahir,
-  "posisi":                  vTPP.JbtnPersonal,
-  "kode_jenjang_pendidikan": kodeJenjangPendidikan,
-  "program_studi":           sql.NullString{},
-  "file_ijazah":             vTPP.PathPersonal,
-  "file_cv":                 sql.NullString{},
- }
+	args := pgx.NamedArgs{
+		"kode_vendor":             helperUser.KodeVendor,
+		"nama":                    vTPP.NmPersonal,
+		"no_ktp":                  sql.NullString{},
+		"file_ktp":                sql.NullString{},
+		"tempat_lahir":            sql.NullString{},
+		"tgl_lahir":               tglLahir,
+		"posisi":                  vTPP.JbtnPersonal,
+		"kode_jenjang_pendidikan": kodeJenjangPendidikan,
+		"program_studi":           sql.NullString{},
+    "file_ijazah":             fileIjazah,
+		"encrypt_key_ijazah":      encryptKeyIjazah,
+		"file_cv":                 sql.NullString{},
+	}
 
- rwIns, errIns := db.DbSidapet.Query(ctx, qIns, args)
- if errIns != nil {
-  log.Fatal("unable to insert ref_tenaga_ahli_bu, " + errIns.Error())
- }
+	rwIns, errIns := db.DbSidapet.Query(ctx, qIns, args)
+	if errIns != nil {
+		log.Fatal("unable to insert ref_tenaga_ahli_bu, " + errIns.Error())
+	}
 
- defer rwIns.Close()
+	defer rwIns.Close()
 
- allRefTenagaAhli, errRwIns := pgx.CollectRows(rwIns, pgx.RowToStructByName[RefTenagaAhli])
- if errRwIns != nil {
-  fmt.Println("nama: " + vTPP.NmPersonal.String + sidapethelper.GetLen(vTPP.NmPersonal.String))
-  fmt.Println("tgl_lahir: " + tglLahir.String + sidapethelper.GetLen(tglLahir.String))
-  fmt.Println("posisi: " + vTPP.JbtnPersonal.String + sidapethelper.GetLen(vTPP.JbtnPersonal.String))
-  fmt.Println("file_ijazah: " + vTPP.PathPersonal.String + sidapethelper.GetLen(vTPP.PathPersonal.String))
-  log.Fatal("failed collecting RefTenagaAhli, " + errRwIns.Error())
- }
+	allRefTenagaAhli, errRwIns := pgx.CollectRows(rwIns, pgx.RowToStructByName[RefTenagaAhli])
+	if errRwIns != nil {
+		fmt.Println("nama: " + vTPP.NmPersonal.String + sidapethelper.GetLen(vTPP.NmPersonal.String))
+		fmt.Println("tgl_lahir: " + tglLahir.String + sidapethelper.GetLen(tglLahir.String))
+		fmt.Println("posisi: " + vTPP.JbtnPersonal.String + sidapethelper.GetLen(vTPP.JbtnPersonal.String))
+		fmt.Println("file_ijazah: " + vTPP.PathPersonal.String + sidapethelper.GetLen(vTPP.PathPersonal.String))
+		log.Fatal("failed collecting RefTenagaAhli, " + errRwIns.Error())
+	}
 
- kodeTenagaAhli := allRefTenagaAhli[0].KodeTenagaAhli.Int32
- rpengalamantamodel.InsertRefPengalamanTa(kodeTenagaAhli, vTPP)
- rsertiftamodel.InserRefSertifTa(kodeTenagaAhli, vTPP)
+	kodeTenagaAhli := allRefTenagaAhli[0].KodeTenagaAhli.Int32
+	rpengalamantamodel.InsertRefPengalamanTa(kodeTenagaAhli, vTPP)
+	rsertiftamodel.InserRefSertifTa(kodeTenagaAhli, vTPP)
 
 }
 
 func InsertRefTenagaPendukungBu(vTPP structs.VmsTblPersonaliaPerush) {
- ctx := context.Background()
+	ctx := context.Background()
 
- var tglLahir pgtype.Text
- strTglLahir, _ := sidapethelper.ConvertToPostgresDate(vTPP.TglPersonal.String)
- if strTglLahir != "" {
-  tglLahir = pgtype.Text{Valid: true, String: strTglLahir}
- }
+	var tglLahir pgtype.Text
+	strTglLahir, _ := sidapethelper.ConvertToPostgresDate(vTPP.TglPersonal.String)
+	if strTglLahir != "" {
+		tglLahir = pgtype.Text{Valid: true, String: strTglLahir}
+	}
 
- kodeJenjangPendidikan := GetKodeJenjangPendidikan(vTPP.PendidikanPersonal)
+	kodeJenjangPendidikan := GetKodeJenjangPendidikan(vTPP.PendidikanPersonal)
 
- qIns := `
+  helperDokumen := helperdokumenmodel.GetByOriginalPath(vTPP.PathPersonal)
+  fileIjazah := helperDokumen.Newfilename
+  encryptKeyIjazah := helperDokumen.EncryptKey
+
+	qIns := `
   INSERT INTO ref_tenaga_pendukung_bu (
     kode_vendor,
     nama,
@@ -175,6 +214,7 @@ func InsertRefTenagaPendukungBu(vTPP structs.VmsTblPersonaliaPerush) {
     kode_jenjang_pendidikan,
     program_studi,
     file_ijazah,
+    encrypt_key_ijazah,
     file_cv
   ) VALUES (
     @kode_vendor,
@@ -187,56 +227,66 @@ func InsertRefTenagaPendukungBu(vTPP structs.VmsTblPersonaliaPerush) {
     @kode_jenjang_pendidikan,
     @program_studi,
     @file_ijazah,
+    @encrypt_key_ijazah,
     @file_cv
   ) RETURNING kode_tenaga_pendukung`
 
- args := pgx.NamedArgs{
-  "kode_vendor":             helperUser.KodeVendor,
-  "nama":                    vTPP.NmPersonal,
-  "no_ktp":                  sql.NullString{},
-  "file_ktp":                sql.NullString{},
-  "tempat_lahir":            sql.NullString{},
-  "tgl_lahir":               tglLahir,
-  "posisi":                  vTPP.JbtnPersonal,
-  "kode_jenjang_pendidikan": kodeJenjangPendidikan,
-  "program_studi":           sql.NullString{},
-  "file_ijazah":             vTPP.PathPersonal,
-  "file_cv":                 sql.NullString{},
- }
+	args := pgx.NamedArgs{
+		"kode_vendor":             helperUser.KodeVendor,
+		"nama":                    vTPP.NmPersonal,
+		"no_ktp":                  sql.NullString{},
+		"file_ktp":                sql.NullString{},
+		"tempat_lahir":            sql.NullString{},
+		"tgl_lahir":               tglLahir,
+		"posisi":                  vTPP.JbtnPersonal,
+		"kode_jenjang_pendidikan": kodeJenjangPendidikan,
+		"program_studi":           sql.NullString{},
+    "file_ijazah":             fileIjazah,
+		"encrypt_key_ijazah":      encryptKeyIjazah,
+		"file_cv":                 sql.NullString{},
+	}
 
- rwIns, err1 := db.DbSidapet.Query(ctx, qIns, args)
- if err1 != nil {
-  fmt.Println("unable to insert ref_tenaga_pendukung_bu, " + err1.Error())
- }
+	rwIns, err1 := db.DbSidapet.Query(ctx, qIns, args)
+	if err1 != nil {
+		fmt.Println("unable to insert ref_tenaga_pendukung_bu, " + err1.Error())
+	}
 
- defer rwIns.Close()
+	defer rwIns.Close()
 
- allTenagaPendukung, err2 := pgx.CollectRows(rwIns, pgx.RowToStructByName[RefTenagaPendukung])
- if err2 != nil {
-  log.Fatal("failed collecting RefTenagaPendukung, " + err2.Error())
- }
+	allTenagaPendukung, err2 := pgx.CollectRows(rwIns, pgx.RowToStructByName[RefTenagaPendukung])
+	if err2 != nil {
+		log.Fatal("failed collecting RefTenagaPendukung, " + err2.Error())
+	}
 
- kodeTenagaPendukung := allTenagaPendukung[0].KodeTenagaPendukung.Int32
- rpengalamantpmodel.InsertRefPengalamanTp(kodeTenagaPendukung, vTPP)
- rsertiftpmodel.InsertRefSertifTp(kodeTenagaPendukung, vTPP)
+	kodeTenagaPendukung := allTenagaPendukung[0].KodeTenagaPendukung.Int32
+	rpengalamantpmodel.InsertRefPengalamanTp(kodeTenagaPendukung, vTPP)
+	rsertiftpmodel.InsertRefSertifTp(kodeTenagaPendukung, vTPP)
 }
 
-
 func GetKodeJenjangPendidikan(pendidikanPersonal pgtype.Text) pgtype.Int4 {
-  var kodeJenjangPendidikan pgtype.Int4
+	var kodeJenjangPendidikan pgtype.Int4
 
-  allHelperJenjangPendidikanKw := helperjenjangpendidikankwmodel.GetAllData()
+	allHelperJenjangPendidikanKw := helperjenjangpendidikankwmodel.GetAllData()
 
-  for _, helperJenjangPendidikanKw := range allHelperJenjangPendidikanKw {
-    // fmt.Printf(helperJenjangPendidikanKw.Keyword.String + "\n")
-    contains := strings.Contains(strings.ToLower(pendidikanPersonal.String), strings.ToLower(helperJenjangPendidikanKw.Keyword.String))
-    if contains {
-      kodeJenjangPendidikan = helperJenjangPendidikanKw.KodeJenjangPendidikan
-      break
+	for _, helperJenjangPendidikanKw := range allHelperJenjangPendidikanKw {
+		// fmt.Printf(helperJenjangPendidikanKw.Keyword.String + "\n")
+
+		str := strings.ToLower(pendidikanPersonal.String)
+		substr := strings.ToLower(helperJenjangPendidikanKw.Keyword.String)
+		contains := strings.Contains(str, substr)
+		if contains {
+			kodeJenjangPendidikan = helperJenjangPendidikanKw.KodeJenjangPendidikan
+			break
+		}
+	}
+
+	if (kodeJenjangPendidikan == pgtype.Int4{}) {
+		//fmt.Printf("IdPersonalia: %d\n", IdPersonalia)
+    // fmt.Printf(pendidikanPersonal.String+" --> %d\n", kodeJenjangPendidikan.Int32)
+    if pendidikanPersonal.String != "-" {
+  		fmt.Printf(pendidikanPersonal.String + "\n")
     }
-  }
+	}
 
-  fmt.Printf(pendidikanPersonal.String + " --> %d\n", kodeJenjangPendidikan.Int32)
-
-  return kodeJenjangPendidikan
+	return kodeJenjangPendidikan
 }
